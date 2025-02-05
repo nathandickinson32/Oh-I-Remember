267236c0ad7f6a23c36736064186250672f4ba3c
nathandickinson32 <nathandickinson32@gmail.com>
2025-02-05 12:03:44 -0500

    sql save

--- Oh-I-Remember/SQL Selects.sql
+++ Oh-I-Remember/SQL Selects.sql
@@ -7,6 +7,9 @@ SELECT * FROM friends;
 SELECT * FROM questions;
 SELECT * FROM notifications;
 
+SELECT COUNT(*) FROM notifications WHERE user_id = 3 AND type = 'friend_request'
+SELECT COUNT(*) FROM notifications WHERE user_id = 3 AND type = 'new_question';
+
 SELECT message FROM messages WHERE room_id = 1 ORDER BY created_at DESC;
 SELECT room_members.user_id FROM users JOIN room_members ON users.user_id = room_members.user_id WHERE room_members.room_id = 1;
 SELECT room_members.user_id FROM users JOIN room_members ON users.user_id = room_members.user_id WHERE room_members.room_id = 3;
