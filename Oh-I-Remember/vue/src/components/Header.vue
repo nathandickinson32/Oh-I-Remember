fa17e8857a68080e646ca365968d0d17f67601ab
nathandickinson32 <nathandickinson32@gmail.com>
2025-02-08 12:02:42 -0500

    added routes to header

--- Oh-I-Remember/vue/src/components/Header.vue
+++ Oh-I-Remember/vue/src/components/Header.vue
@@ -11,6 +11,17 @@
     <span v-if="route.name ==='edit-question'"> | </span>
     <router-link v-if="route.name ==='edit-question'" :to="{name: 'do-you-remember'}"> Back to Questions </router-link>
 
+    <span v-if="route.name ==='friend-requests'"> | </span>
+    <router-link v-if="route.name ==='friend-requests'" :to="{name: 'friends-list'}"> Back to Friends List </router-link>
+
+
+    <span v-if="route.name ==='ask-question-view'"> | </span>
+    <router-link v-if="route.name ==='ask-question-view'" :to="{name: 'do-you-remember'}"> Back to Questions </router-link>
+
+
+      <span v-if="route.name ==='ask-question-by-id-view'"> | </span>
+    <router-link v-if="route.name ==='ask-question-by-id-view'" :to="{name: 'friends-list'}"> Back to Friends List </router-link>
+
     <span> | </span>
     <router-link v-bind:to="{ name: 'logout' }">Logout</router-link>
 
