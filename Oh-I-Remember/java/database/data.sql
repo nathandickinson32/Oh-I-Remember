0f777b0ead4d911a87fb48592af04fa81dcab6b6
nathandickinson32 <nathandickinson32@gmail.com>
2025-02-25 09:09:21 -0500

    saving

--- Oh-I-Remember/java/database/data.sql
+++ Oh-I-Remember/java/database/data.sql
@@ -1,4 +1,4 @@
-BEGIN TRANSACTION;
+--BEGIN TRANSACTION;
 --
 --INSERT INTO users (username,password_hash,role,first_name, last_name) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'Bob', 'Boberson');
 --INSERT INTO users (username,password_hash,role,first_name, last_name) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Christie', 'Dickinson');
@@ -28,4 +28,4 @@ BEGIN TRANSACTION;
 --VALUES ('Pending'), ('Accepted'), ('Denied');
 
 
-COMMIT TRANSACTION;
+--COMMIT TRANSACTION;
